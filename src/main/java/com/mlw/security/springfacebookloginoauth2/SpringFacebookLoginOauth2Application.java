facfe05573b22444153b2b104c181f2b3bcabd59
default <lipika.jk@gmail.com>
2020-11-02 21:26:39 +0100

    Initial commit

---
+++ src/main/java/com/mlw/security/springfacebookloginoauth2/SpringFacebookLoginOauth2Application.java
@@ -0,0 +1,13 @@
+package com.mlw.security.springfacebookloginoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFacebookLoginOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFacebookLoginOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlw/security/springfacebookloginoauth2/SpringFacebookLoginOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlw.security.springfacebookloginoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFacebookLoginOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
